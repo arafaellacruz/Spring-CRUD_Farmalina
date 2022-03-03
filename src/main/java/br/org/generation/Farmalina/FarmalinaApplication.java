58aff06e20d972b2783c1c0c1b22c91dd4a39bf8
Rafaella <rafaella.leticiaa@gmail.com>
2022-03-03 17:37:58 -0300

    Exercício de revisão Spring CRUD

---
+++ src/main/java/br/org/generation/Farmalina/FarmalinaApplication.java
@@ -0,0 +1,13 @@
+package br.org.generation.Farmalina;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FarmalinaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FarmalinaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/org/generation/Farmalina/FarmalinaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.org.generation.Farmalina;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FarmalinaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
